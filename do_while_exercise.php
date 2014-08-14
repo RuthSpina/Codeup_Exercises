7febfe605605ebad00f2d446ea47e51877b92881
Ruth Spina <ruthspina@Ruths-MacBook-Air.local>
2014-08-14 10:39:38 -0500

    exercise

--- do_while_exercise.php
+++ do_while_exercise.php
@@ -1,7 +1,18 @@
 <?php
 
-$a = 1;
+// $a = 0;
+// do {
+//     echo "{$a} is equal to {$a}\n"; $a+=2;
+// } while ($a <= 100);
+
+$a = 100;
 do {
-    echo "\$a is equal to {$a}\n";
-    $a++;
-} while (1 == 2);
+	echo "\$a is equal to {$a}\n"; 
+	$a -= 5;
+} while ($a >= -10);
+
+// $a = 2;
+// do {
+// 		echo "\$a is equal to {$a}\n";
+// 		$a *= $a;
+// 	} while ($a <= 1000000)
