d6648c8380bb799d7eecefc1790b40478878616e
Ruth Spina <ruthspina@Ruths-MacBook-Air.local>
2014-08-18 13:46:07 -0500

    update

--- hw_wk1_p2.php
+++ hw_wk1_p2.php
@@ -1,49 +1,53 @@
 <?php
 
 $quiz = array(
-	'Where do you live?' => array(
+	'question' =>'Where do you live?'array(
 		'a' => 'San Antonio, TX',
 		'b' => 'Las Vegas, NV',
 		'c' => 'Austin, TX',
 		'd' => 'New York, NY' 
 		),
 		'correct_answer' => 'a'
-	'What is your first name?' => array(
+	'question' => 'What is your first name?' array(
 		'a' => 'Betty', 
 		'b' => 'Susan',
 		'c' => 'Ruth',
 		'd' => 'Michelle'
 		),
-		'correct_answer' => 'c'
-	'What is your last name?' => array(
+		'correct_answer' => 'c',
+	'question' => 'What is your last name?' array(
 		'a' => 'Smith',
 		'b' => 'Arnold',
 		'c' => 'Brennan',
 		'd' => 'Spina'
 		),
-	'correct_answer' => 'd' 
-	'What is your favorite color?' => array(
+	'correct_answer' => 'd' ,
+	'question' => 'What is your favorite color?' array(
 		'a' => 'Blue',
 		'b' => 'Red',
 		'c' => 'Orange',
 		'd' => 'Purple'
 		),
-	'correct_answer'=> 'a'
+	'correct_answer'=> 'a',
 
 	);
-		
+	$question_number++;	
 						
-	foreach($quiz as $question => $answer) {
-		//$question_number. {$value['question']};
+	foreach($quiz as $question => $value) {
 
-		//$correct_answer = "{$value['correct_answer']}";
-		//foreach ($value['answers'] as $answer_choice => $answer_content) {
-		//if ($answerchoice == $correctanswer);
+		echo "$question_number. {$value['question']}";
+
+		$correct_answer = "{$value['correct_answer']}";
+
+		foreach ($value['answers'] as $answer_choice => $answer_content) {
+
+		if ($answerchoice == $correctanswer);
 	}
-		//echo "\t* $answerchoice $answercontent . EOL.PHP;"
-		//else{
-		//"\t $answerchoice $answercontent . EOL.PHP;"
-	}
+		echo "\t* $answerchoice $answercontent . EOL.PHP;"
+		
+	else{
+		"\t $answerchoice $answercontent . EOL.PHP;"
+	
 	}
 
 	for($value = 1; $value <= count($question); $value++) {
