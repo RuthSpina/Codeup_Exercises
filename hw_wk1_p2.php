295942ee5462c5aa5bd972a0c1bc669009b83a8f
Ruth Spina <ruthspina@Ruths-MacBook-Air.local>
2014-08-18 12:07:33 -0500

    first commit

--- hw_wk1_p2.php
+++ hw_wk1_p2.php
@@ -7,29 +7,44 @@ $quiz = array(
 		'c' => 'Austin, TX',
 		'd' => 'New York, NY' 
 		),
+		'correct_answer' => 'a'
 	'What is your first name?' => array(
 		'a' => 'Betty', 
 		'b' => 'Susan',
 		'c' => 'Ruth',
 		'd' => 'Michelle'
 		),
+		'correct_answer' => 'c'
 	'What is your last name?' => array(
 		'a' => 'Smith',
 		'b' => 'Arnold',
 		'c' => 'Brennan',
 		'd' => 'Spina'
 		),
+	'correct_answer' => 'd' 
 	'What is your favorite color?' => array(
 		'a' => 'Blue',
 		'b' => 'Red',
 		'c' => 'Orange',
 		'd' => 'Purple'
 		),
+	'correct_answer'=> 'a'
 
 	);
 		
 						
 	foreach($quiz as $question => $answer) {
+		//$question_number. {$value['question']};
+
+		//$correct_answer = "{$value['correct_answer']}";
+		//foreach ($value['answers'] as $answer_choice => $answer_content) {
+		//if ($answerchoice == $correctanswer);
+	}
+		//echo "\t* $answerchoice $answercontent . EOL.PHP;"
+		//else{
+		//"\t $answerchoice $answercontent . EOL.PHP;"
+	}
+	}
 
 	for($value = 1; $value <= count($question); $value++) {
 		echo "{$value}. {$question}\n\n";		
@@ -40,7 +55,7 @@ $quiz = array(
 
 	  }
 	}
-	
+	//correct answer should be added as answer option.
 
 
 	?>
