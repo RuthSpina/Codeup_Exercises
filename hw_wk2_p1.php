43e63b3d913b6094ca42b1d9500a54e591221857
Ruth Spina <ruthspina@Ruths-MacBook-Air.local>
2014-08-25 09:55:01 -0500

    update

---
+++ hw_wk2_p1.php
@@ -0,0 +1,61 @@
+
+<?php
+
+for($i = 0, $i <= 12, $i++)
+$month_name = date()	
+$num = cal_days_in_month(CAL_GREGORIAN, 8, 2014); // 31
+echo "There was $num days in January 2014";
+
+////
+
+
+////
+
+$cars = array(
+    'Toyota Solara' => array(
+        'year' => 2001,
+        'doors' => 2,
+        'color' => 'gray',
+        'mileage' => 100000,
+        'sunroof' => true,
+        'convertable' = false,
+        'license' => 'JSRULZ',
+    ),
+
+    'Honda Odyssey' => array(
+        'year' => 2004, 
+        'doors' => 4,
+        'color' => 'marron',
+        'mileage' => 60000,
+        'sunroof' => false,
+        'convertable' = false,
+        'license' => 'ILUVPHP',
+    ),
+
+    'Cadillac Escalade' => array(
+        'year' => 2014,
+        'doors' => 4,
+        'color' => 'white',
+        'mileage' => 5000,
+        'sunroof' => false,
+        'convertable' = false,
+        'license' => 'HTML5',
+    ),
+
+);
+
+function car_id($cars){
+	foreach($make_model as $attributes){
+
+
+	}
+} 
+
+///add car to $cars array.
+function add_car(){
+
+}
+
+
+
+?>
