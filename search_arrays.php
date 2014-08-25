dbef9ea745cbe17586322fdd662b3a47b41d9798
Ruth Spina <ruthspina@Ruths-MacBook-Air.local>
2014-08-25 09:55:38 -0500

    update

---
+++ search_arrays.php
@@ -0,0 +1,70 @@
+
+<?php
+
+ // first names
+ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+
+ function true_false($input, $array) {
+
+ $result = array_search($input, $array);
+//$answer = var_dump((bool)$result);
+ if ($answer){
+	
+ 	echo 'TRUE' . PHP_EOL;
+ 	//echo 'True - name is found' . PHP_EOL;
+
+ }else{
+ 	 echo 'FALSE' . PHP_EOL;
+ 	//var_dump((bool)$result);
+ 	//echo 'False - name is found' . PHP_EOL;
+ }
+}
+
+function array_compare($array1, $array2){
+	$i = 0;
+	foreach($array1 as $name){
+		$answer = (true_false($name, $array2));
+		$result = array_search()
+		if($answer){
+			
+			echo "True" . PHP_EOL;
+			
+		}else{
+			$i++;
+			return $answer;
+			//return "FALSE" . PHP_EOL;
+				
+		}
+	
+	}
+}
+
+	
+
+
+$result = array_compare($names, $compare);
+echo "$result" . PHP_EOL;
+
+
+
+ // $answer = true_false('Ruth', $names);
+ // return "$answer" . PHP_EOL;
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
